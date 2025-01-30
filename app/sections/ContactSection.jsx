b762e5f858b02f021b0e29532d531cf75062bc2c
jkClyde <aquin.randall@gmail.com>
2025-01-31 07:37:19 +0800

    contact-form responsive

--- app/sections/ContactSection.jsx
+++ app/sections/ContactSection.jsx
@@ -4,12 +4,12 @@ const ContactSection = () => {
     return (
       <div className="inner-row mt-[60px] md:mt-[111px] text-center mb-[60px] md:mb-[111px]">
         <div className="bg-[#211E2E] rounded-[40px] px-[15px] md:px-[110px] w-full mx-auto pt-[25px] md:pt-[90px] pb-[25px] md:pb-[84px]">
-          <h2 className="text-h2 font-bold mb-[30px]">JOIN HYDRA</h2>
-          <p className="text-h2 font-light mb-[25px] md:mb-[65px]">Let's Build Your VR Experience</p>
+          <h2 className="text-h3 md:text-h2 font-bold mb-[30px]">JOIN HYDRA</h2>
+          <p className="text-h3 md:text-h2 font-light mb-[25px] md:mb-[65px]">Let's Build Your VR Experience</p>
           
           <form className="max-w-[1058px] mx-auto">
             {/* Name Fields Row */}
-            <div className="flex flex-col md:flex-row gap-4  mb-[15px] md:mb-[39px]">
+            <div className="flex flex-col md:flex-row gap-4  mb-[22px] md:mb-[39px]">
               <input
                 type="text"
                 placeholder="First Name"
@@ -23,7 +23,7 @@ const ContactSection = () => {
             </div>
   
             {/* Email and Phone Row */}
-            <div className="flex flex-col md:flex-row gap-4 mb-[15px] md:mb-[39px]">
+            <div className="flex flex-col md:flex-row gap-4 mb-[22px] md:mb-[39px]">
               <input
                 type="email"
                 placeholder="Email"
@@ -40,14 +40,14 @@ const ContactSection = () => {
             <input
               type="text"
               placeholder="Subject"
-              className="w-full bg-transparent border border-[#FFFFFF] rounded-full py-4 px-6 text-white placeholder-gray-400 focus:outline-none focus:border-[#8176AF] mb-[15px] md:mb-[39px]"
+              className="w-full bg-transparent border border-[#FFFFFF] rounded-full py-4 px-6 text-white placeholder-gray-400 focus:outline-none focus:border-[#8176AF] mb-[22px] md:mb-[39px]"
             />
   
             {/* Message Field */}
             <textarea
               placeholder="Tell Us Something..."
               rows={6}
-              className="w-full bg-transparent border border-[#FFFFFF] rounded-[40px] py-4 px-6 text-white placeholder-gray-400 focus:outline-none focus:border-[#8176AF] mb-[15px] md:mb-[51px] resize-none"
+              className="w-full bg-transparent border border-[#FFFFFF] rounded-[40px] py-4 px-6 text-white placeholder-gray-400 focus:outline-none focus:border-[#8176AF] mb-[22px] md:mb-[51px] resize-none"
             />
   
             {/* Submit Button */}
