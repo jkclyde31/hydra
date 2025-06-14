9946b7aaa302c08717d7406c4dec35b5ee3f764d
jkclyde31 <randal.technodream@gmail.com>
2025-06-15 04:10:58 +0800

    Update navLinks.js

--- components/Navbar/navLinks.js
+++ components/Navbar/navLinks.js
@@ -1,7 +1,7 @@
 export const navLinks = [
     { name: 'Home', href: '/' },
-    { name: 'About Us', href: '/about' },
-    { name: 'Services', href: '/services' },
-    { name: 'Reviews', href: '/reviews' },
-    { name: 'Contact Us', href: '/contact' },
+    { name: 'About Us', href: '#' },
+    { name: 'Services', href: '#' },
+    { name: 'Reviews', href: '#' },
+    { name: 'Contact Us', href: '#' },
   ];
